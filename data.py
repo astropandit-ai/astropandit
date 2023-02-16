b136c6984e2013eef8e0b1db7b0bd92c76128d63
astropandit-ai <125592928+astropandit-ai@users.noreply.github.com>
2023-02-16 23:13:36 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+astrology_data = {
+    'aries': {
+        'element': 'fire',
+        'planet': 'Mars',
+        'color': 'red',
+        'horoscope': 'You will experience a burst of energy today.',
+    },
+    'taurus': {
+        'element': 'earth',
+        'planet': 'Venus',
+        'color': 'green',
+        'horoscope': 'You will have a productive day at work.',
+    },
+    # ... other zodiac signs ...
+}
