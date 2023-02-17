827bfeb29e74b6b602567798e4c91b25b1cd2821
astropandit-ai <125592928+astropandit-ai@users.noreply.github.com>
2023-02-17 15:51:21 +0530

    Delete sam.js

--- sam.js
+++
@@ -1,3 +0,0 @@
-const chatArea = document.getElementById('chat-area');
-chatArea.innerHTML += '<div class="message user-message">' + message + '</div>';
-chatArea.innerHTML += '<div class="message bot-message">' + result.fulfillmentText + '</div>';
