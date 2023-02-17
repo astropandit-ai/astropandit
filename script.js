d2cf148effd1a26cf8453341cfc34e7038c2d225
astropandit-ai <125592928+astropandit-ai@users.noreply.github.com>
2023-02-17 15:51:29 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const dialogflow = require('dialogflow');
-const projectId = 'astropandit-fkww'; // Replace with your own project ID
-const sessionId = 'astropandit-fkww'; // Replace with a unique session ID
-const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
